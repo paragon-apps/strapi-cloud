716fc0d6b3e905aae333f4549939ac1db265ac5b
Darshan Parmar <dparmar@paragonsoft.co>
2023-12-21 15:55:44 +0530

    🚧 🐛  fix: partner slider new version added

---
+++ src/api/home-page-partner-slider/routes/home-page-partner-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-partner-slider router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-page-partner-slider.home-page-partner-slider');
