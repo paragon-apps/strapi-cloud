8c480d6249ca894a2f45683694b7177f2de1c708
Darshan Parmar <dparmar@paragonsoft.co>
2024-01-16 18:46:25 +0530

    🌱   Home stories updated version

---
+++ src/api/home-story/routes/home-story.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-story router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-story.home-story');
