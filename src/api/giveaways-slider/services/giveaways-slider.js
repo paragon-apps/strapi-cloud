325fcae6298bc3451b529bc1c9c5a15cef62b469
Darshan Parmar <dparmar@paragonsoft.co>
2024-01-22 19:20:36 +0530

    ⚡️ New single type added for Giveaways slider

---
+++ src/api/giveaways-slider/services/giveaways-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * giveaways-slider service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::giveaways-slider.giveaways-slider');
