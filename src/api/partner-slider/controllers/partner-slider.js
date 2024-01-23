fce1d2bc3ad424c47d3d286801a63601175d4cf0
Darshan Parmar <dparmar@paragonsoft.co>
2024-01-23 11:21:03 +0530

    🗑️ removed old partner slide single type + components

--- src/api/partner-slider/controllers/partner-slider.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * partner-slider controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::partner-slider.partner-slider');
--- src/api/partner-slider/services/partner-slider.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * partner-slider service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::partner-slider.partner-slider');
